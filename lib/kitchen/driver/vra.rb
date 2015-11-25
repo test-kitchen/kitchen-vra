32fde690d2f196ea73183b3972fdbab648a7578c
Adam Leff <adamleff@chef.io>
2015-11-25 14:36:31 -0500

    Adding sleep-before-ready config, and better failback for hostname.
    In some environments, a race condition can exist between when a vRA
    instance is provisioned to when it is actually ready. Such a race
    condition may be DNS propagation. In the event of a NXDOMAIN when
    TK looks up a hostname, it will cause TK to throw an exception
    rather than waiting and trying again (as TK assumes a DNS failure
    is unrecoverable). The `server_ready_sleep_time` allows users to
    configure a sleep time before TK checks to see if the host is ready.
    This is a hack, but it's an easy one.
    
    Additionally, when `use_dns` is false, the vRA driver will still
    fallback to the hostname in the event an IP address is not found.
    This avoids the user needing to set use_dns to true in their
    environment in these cases.

--- lib/kitchen/driver/vra.rb
+++ lib/kitchen/driver/vra.rb
@@ -22,7 +22,7 @@ require_relative 'vra_version'
 
 module Kitchen
   module Driver
-    class Vra < Kitchen::Driver::Base
+    class Vra < Kitchen::Driver::Base # rubocop:disable Metrics/ClassLength
       kitchen_driver_api_version 2
       plugin_version Kitchen::Driver::VRA_VERSION
 
@@ -36,6 +36,7 @@ module Kitchen
       default_config :verify_ssl, true
       default_config :request_timeout, 600
       default_config :request_refresh_rate, 2
+      default_config :server_ready_sleep_time, nil
       default_config :cpus, 1
       default_config :memory, 1024
       default_config :requested_for do |driver|
@@ -61,17 +62,27 @@ module Kitchen
 
         server = request_server
         state[:resource_id] = server.id
-        if config[:use_dns]
-          raise 'No server name returned for the vRA request' if server.name.nil?
-          state[:hostname] = server.name
-        else
-          raise 'No IP address returned for the vRA request' if server.ip_addresses.first.nil?
-          state[:hostname] = server.ip_addresses.first
-        end
-        state[:ssh_key] = config[:private_key_path] unless config[:private_key_path].nil?
+        state[:hostname]    = hostname_for(server)
+        state[:ssh_key]     = config[:private_key_path] unless config[:private_key_path].nil?
 
+        sleep_for_server(server)
         wait_for_server(state, server)
         info("Server #{server.id} (#{server.name}) ready.")
+      end
+
+      def hostname_for(server)
+        if config[:use_dns]
+          raise 'No server name returned for the vRA request' if server.name.nil?
+          return server.name
+        end
+
+        ip_address = server.ip_addresses.first
+        if ip_address.nil?
+          warn("Server #{server.id} has no IP address. Falling back to server name (#{server.name})...")
+          server.name
+        else
+          ip_address
+        end
       end
 
       def request_server
@@ -87,6 +98,14 @@ module Kitchen
         raise 'the vRA request did not create any servers.' if servers.size == 0
 
         servers.first
+      end
+
+      def sleep_for_server(server)
+        sleep_length = config[:server_ready_sleep_time]
+        return unless sleep_length
+
+        info("Sleeping #{sleep_length} seconds before checking to see if server #{server.id} (#{server.name}) is ready...")
+        sleep sleep_length
       end
 
       def wait_for_server(state, server)
